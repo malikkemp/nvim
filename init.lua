a0044d42814bde0e019bf8dd4d9a9b97386c6bf8
malikkemp <malikkemp1152@gmail.com>
2024-04-15 22:34:02 -0400

    Updating from Laptop

--- init.lua
+++ init.lua
@@ -17,12 +17,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Plugins
 local plugins = {
   { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
@@ -30,9 +32,16 @@ require("lazy").setup(plugins, opts)
 
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n','<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 
--- Default options for gruvbox plugin:
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript", "python", "c", "vim"},
+  highlight = {enable = true},
+  indent = {enable = true},
+})
+
+-- Default options for gruvbox theme plugin:
 require("gruvbox").setup({
   terminal_colors = true, -- add neovim terminal colors
   undercurl = true,
@@ -56,6 +65,6 @@ require("gruvbox").setup({
   overrides = {},
   dim_inactive = false,
   transparent_mode = false,
-  })
-vim.o.background = "dark" -- "dark or light
+})
+vim.o.background = "dark" -- "dark" or "light" for light mode
 vim.cmd("colorscheme gruvbox")
